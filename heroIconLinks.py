aea3308a1d6f334a07f116168c9514f08bd5c254
alexanderrotariu <alexanderrotariu@yahoo.com>
2022-02-17 17:30:50 -0500

    added heroIconList and cleaned up defs file

---
+++ heroIconLinks.py
@@ -0,0 +1,39 @@
+heroUrl= {
+
+    #there is a way of making this cleaner but i dont want to deal with sending file paths over different python scripts so no thanks ill stick to url fetching 
+
+        #DPS HERO LINKS 
+        'Ashe': "https://cdn.discordapp.com/attachments/737088061250207859/942213813774716958/Icon-Ashe.png",
+        'Bastion': "https://cdn.discordapp.com/attachments/737088061250207859/942213939641585704/Icon-Bastion.png",
+        'Doomfist':"https://cdn.discordapp.com/attachments/737088061250207859/942213940090392576/Icon-Doomfist.png",
+        'Echo':"https://cdn.discordapp.com/attachments/737088061250207859/942213940610465822/Icon-Echo.png", 
+        'Genji':"https://cdn.discordapp.com/attachments/737088061250207859/942213940795047996/Icon-Genji.png",
+        'Hanzo':"https://cdn.discordapp.com/attachments/737088061250207859/942213939167649792/Icon-Hanzo.png",
+        'Junkrat':"https://cdn.discordapp.com/attachments/737088061250207859/942214168377962557/Icon-Junkrat.png",
+        'Cassidy':"https://cdn.discordapp.com/attachments/737088061250207859/942217757578121286/Icon-Cassidy.png",
+        'Mei':"https://cdn.discordapp.com/attachments/737088061250207859/942214168935800862/Icon-Mei.png",
+        'Reaper':"https://cdn.discordapp.com/attachments/737088061250207859/942214170575790090/Icon-Reaper.png",
+        'Soldier76':"https://cdn.discordapp.com/attachments/737088061250207859/942214212904689694/Icon-Soldier_76.png",
+        'Sombra':"https://cdn.discordapp.com/attachments/737088061250207859/942214213223469096/Icon-Sombra.png",
+        'Symmetra':"https://cdn.discordapp.com/attachments/737088061250207859/942214213416398868/Icon-Symmetra.png",
+        'Torbjorn':"https://cdn.discordapp.com/attachments/737088061250207859/942214213739372604/Icon-Torbjorn.png",
+        'Tracer':"https://cdn.discordapp.com/attachments/737088061250207859/942214213999403038/Icon-Tracer.png",
+        'Widowmaker':"https://cdn.discordapp.com/attachments/737088061250207859/942214214230081556/Icon-Widowmaker.png",
+        #TANK HERO LINKS 
+        'Dva':"https://cdn.discordapp.com/attachments/737088061250207859/942213940383989801/Icon-Dva.png",
+        'Reinhardt':"https://cdn.discordapp.com/attachments/737088061250207859/942214171011973120/Icon-Reinhardt.png",
+        'Roadhog':"https://cdn.discordapp.com/attachments/737088061250207859/942214212334256139/Icon-Roadhog.png",
+        'Sigma':"https://cdn.discordapp.com/attachments/737088061250207859/942214212623695944/Icon-Sigma.png",
+        'Winston':"https://cdn.discordapp.com/attachments/737088061250207859/942214214527909979/Icon-Winston.png",
+        'Wreckingball':"https://cdn.discordapp.com/attachments/737088061250207859/942214228960501790/Icon-Wrecking_Ball.png",
+        'Zarya':"https://cdn.discordapp.com/attachments/737088061250207859/942214229157609502/Icon-Zarya.png",
+        'Orisa':"https://cdn.discordapp.com/attachments/737088061250207859/942214169904685106/Icon-Orisa.png",
+        #SUPPORT HERO LINKS     
+        'Ana':"https://cdn.discordapp.com/attachments/737088061250207859/942213722011742218/Icon-Ana.png",
+        'Baptiste':"https://cdn.discordapp.com/attachments/737088061250207859/942213939406716948/Icon-Baptiste.png",
+        'Brigitte':"https://cdn.discordapp.com/attachments/737088061250207859/942213939859718164/Icon-Brigitte.png",
+        'Lucio':"https://cdn.discordapp.com/attachments/737088061250207859/942214168646418512/Icon-Lucio.png",
+        'Mercy':"https://cdn.discordapp.com/attachments/737088061250207859/942214169162309672/Icon-Mercy.png",
+        'Moira':"https://cdn.discordapp.com/attachments/737088061250207859/942214169577533500/Icon-Moira.png",
+        'Zenyatta':"https://cdn.discordapp.com/attachments/737088061250207859/942214229337976912/Icon-Zenyatta.png"
+    }
